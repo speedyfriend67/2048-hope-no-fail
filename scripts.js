a9aeb8e7137ba1539cb2369a9d3369ce7c586ff9
Speedyfriend67 <82425907+speedyfriend67@users.noreply.github.com>
2023-12-23 17:32:54 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,96 +1,40 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const gridContainer = document.getElementById('grid-container');
-  const scoreElement = document.getElementById('score');
-  const restartButton = document.getElementById('restart-button');
-  const gameOverOverlay = document.getElementById('game-over-overlay');
-  const gameOverMessage = document.getElementById('game-over-message');
+    const gridSize = 4;
+    const gridContainer = document.getElementById('grid-container');
+    let grid = Array.from({ length: gridSize }, () => Array(gridSize).fill(0));
 
-  let board = [];
-  let score = 0;
-
-  // Function to initialize the game board
-  function initBoard() {
-    board = [
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0],
-      [0, 0, 0, 0]
-    ];
-    score = 0;
-    scoreElement.textContent = 'Score: 0';
-    gameOverOverlay.style.display = 'none';
-    gameOverMessage.textContent = 'Game Over!';
-    createTiles();
-    addRandomNumber();
-    addRandomNumber();
-  }
-
-  // Function to create the tiles on the grid
-  function createTiles() {
-    gridContainer.innerHTML = '';
-    for (let row = 0; row < 4; row++) {
-      for (let col = 0; col < 4; col++) {
-        const tile = document.createElement('div');
-        tile.className = 'grid-cell';
-        tile.textContent = board[row][col] !== 0 ? board[row][col] : '';
-        gridContainer.appendChild(tile);
-      }
-    }
-  }
-
-  // Function to generate a new random number (2 or 4)
-  function generateRandomNumber() {
-    return Math.random() < 0.9 ? 2 : 4;
-  }
-
-  // Function to add a new random number to the board
-  function addRandomNumber() {
-    const emptyCells = [];
-    for (let row = 0; row < 4; row++) {
-      for (let col = 0; col < 4; col++) {
-        if (board[row][col] === 0) {
-          emptyCells.push({ row, col });
-        }
-      }
+    function initialize() {
+        // Your initialization logic here
     }
 
-    if (emptyCells.length > 0) {
-      const randomCell = emptyCells[Math.floor(Math.random() * emptyCells.length)];
-      board[randomCell.row][randomCell.col] = generateRandomNumber();
-      updateTiles();
+    function render() {
+        gridContainer.innerHTML = '';
+        // Your rendering logic here
     }
-  }
 
-  // Function to update the tiles on the grid
-  function updateTiles() {
-    const tiles = document.getElementsByClassName('grid-cell');
-    for (let row = 0; row < 4; row++) {
-      for (let col = 0; col < 4; col++) {
-        const tile = tiles[row * 4 + col];
-        tile.textContent = board[row][col] !== 0 ? board[row][col] : '';
-        tile.className = 'grid-cell';
-        tile.classList.add(`tile-${board[row][col]}`);
-      }
+    function handleSwipe(direction) {
+        // Handle swipe in the specified direction
+        // Update grid and call render function
     }
-    scoreElement.textContent = `Score: ${score}`;
-  }
 
-  // Function to move the tiles in a given direction
-  function moveTiles(direction) {
-    // Implement the logic to move the tiles in the specified direction
+    function handleKeyPress(event) {
+        // Handle key press for arrow keys
+        // Call handleSwipe with the corresponding direction
+    }
 
-    // After moving the tiles, call addRandomNumber() to add a new random number
-    addRandomNumber();
-  }
+    function isGameOver() {
+        // Check if the game is over
+    }
 
-  // Function to check if the game is over (no more valid moves)
-  function isGameOver() {
-    // Implement the logic to check if the game is over
-  }
+    // Initialize the game
+    initialize();
+    render();
 
-  // Event listener for arrow key presses
-  document.addEventListener('keydown', (event) => {
-    if (event.key === 'ArrowUp') {
+    // Event listeners for swipe and key press
+    // Add event listeners for swipe gestures on mobile devices
+    // Add event listener for arrow keys on desktop
+
+});
       moveTiles('up');
     } else if (event.key === 'ArrowDown') {
       moveTiles('down');
